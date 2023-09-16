f8c672922c8e49ad1bbf4ce4ca213040a8623636
hunterpiggot <hunterpiggot@gmail.com>
2023-09-15 19:08:43 -0600

    Fixed some styling

--- src/components/home-page/about-me/AboutMe.tsx
+++ src/components/home-page/about-me/AboutMe.tsx
@@ -2,11 +2,11 @@ import fakeReactCode from "../../../../public/assets/fake-react-code.jpg";
 
 export const AboutMe = () => {
   return (
-    <div className="flex flex-col-reverse h-screen px-12 py-24 bg-custom-white xl:flex-row sm:gap-12 md:gap-16 lg:gap-20 xl:gap-24">
+    <div className="flex flex-col-reverse min-h-screen px-12 py-24 bg-custom-white xl:flex-row sm:gap-12 md:gap-16 lg:gap-20 xl:gap-24">
       <div className="flex-grow order-2 w-full text-center xl:order-2">
         <div className="text-c-xl">About me</div>
         <div className="w-full h-1 bg-custom-teal"></div>
-        <div className="pt-4 text-lg sm:text-md md:text-lg lg:text-lg 2xl:text-xl 3xl:text-2xl">
+        <div className="flex-grow pt-4 text-sm sm:text-md md:text-lg lg:text-lg 2xl:text-xl 3xl:text-2xl">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus nostrum
           voluptatum in voluptatem, corrupti error sint ratione neque cumque eum
           officiis ullam temporibus, pariatur necessitatibus velit dolore.
@@ -23,12 +23,14 @@ export const AboutMe = () => {
           quod saepe est.
         </div>
       </div>
-      <div className="flex-grow flex-shrink order-1 hidden w-full min-w-0 h-1/2 xl:h-full sm:block xl:w-1/3 xl:order-1">
-        <img
-          className="object-cover w-full h-full"
-          src={fakeReactCode}
-          alt=""
-        />
+      <div className="flex">
+        <div className="flex-grow flex-shrink order-1 hidden w-full min-w-0 h-1/2 xl:h-full sm:block xl:w-1/3 xl:order-1">
+          <img
+            className="object-cover w-full h-full"
+            src={fakeReactCode}
+            alt=""
+          />
+        </div>
       </div>
     </div>
   );
--- src/primitives/buttons/BaseButton.primitive.tsx
+++ src/primitives/buttons/BaseButton.primitive.tsx
@@ -2,7 +2,7 @@ import { MouseEvent } from "react";
 
 interface Props {
   label: string;
-  onClick: Function;
+  onClick: (e: MouseEvent<HTMLDivElement>) => void;
   color: "white" | "black";
   disabled?: boolean;
   extraCss?: string;
@@ -11,12 +11,14 @@ interface Props {
 }
 export const BaseButton = (props: Props) => {
   const { label, onClick, color } = props;
-  const handleClick = (e: MouseEvent<HTMLButtonElement>) => {
+  const handleClick = (e: MouseEvent<HTMLDivElement>) => {
     onClick(e);
   };
 
   const getBaseCss = (): string => {
-    const baseClass = ["px-2.5 py-2 text-c-lg rounded-2xl text-center"];
+    const baseClass = [
+      "cursor-pointer px-2.5 py-2 text-c-lg rounded-2xl text-center",
+    ];
     if (color === "black") {
       baseClass.push("bg-custom-black text-custom-white");
     } else {
@@ -35,11 +37,11 @@ export const BaseButton = (props: Props) => {
     <div
       data-testid={props.testid ? props.testid : "not-set"}
       className={getBaseCss()}
+      onClick={handleClick}
     >
       <button
         data-testid={props.testid ? `${props.testid}.button` : "not-set"}
         disabled={props.disabled ? true : false}
-        onClick={handleClick}
       >
         {label}
       </button>
